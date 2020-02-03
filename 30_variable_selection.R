65e73eb6a52b7fcfa5265a757e6ec5f0496e018a
Anthony Doan <mythicalprogrammer@gmail.com>
2020-02-02 19:52:02 -0800

    Finished the 2nd part of variable selection algorithm.

--- 30_variable_selection.R
+++ 30_variable_selection.R
@@ -19,7 +19,7 @@ Define α=0.05/K and β=0.05/{K(K−1)}:
          pair  with  the  largest  value  of W_I(X_i,X_j) and exit.
     (b)  Otherwise, select the X_i with the largest value of W_M(X_i)."
 
-K <- ncol(mammograph_data) - 1 (response) #TODO automate this via formula
+K <- ncol(mammograph_data) - 1 #(response) #TODO automate this via formula
 alpha <- 0.05/K
 
 # 1.  Use Procedure 2.1 to find W_M(X_i) for i=1,2,...,K.
@@ -34,4 +34,31 @@ for (i in 1:ncol(mammograph_data)) {
     list_main_effect[[j]] <- c(names(curr_pred), chi_sq)
     j = j + 1
   }
+}
+
+
+#2.  If max_i W_M(X_i) > χ2_(1,α),
+#    select the variable with the largest value of W_M(X_i) and exit.
+curr_max <- 0
+for (i in 1:length(list_main_effect)) {
+  curr_list <- list_main_effect[[i]]
+  if (curr_list[2] > curr_max) {
+    curr_max <- list_main_effect[i]
+  }
+}
+
+# TODO: Make sure this is correct
+# https://stackoverflow.com/questions/60032887/how-do-i-find-the-value-of-chi-square-of-alpha-0-05-and-degree-of-freedom-of-1-i
+chi_sq_alpha_1df <- dchisq(1-alpha, df=1)
+
+chosen_var <- c()
+if (as.numeric(curr_max[[1]][2]) > chi_sq_alpha_1df) {
+  chosen_var <- curr_max[[1]][1]
+} else {
+"
+3.  Otherwise,  use  Procedure 2.2 to  find W_I(X_i,X_j)
+for  each  pair  of  predictor variables:
+  (a)  If  max_(i!=j) W_I(X_i,X_j) >χ2_(1,β), select  the
+pair  with  the  largest  value  of W_I(X_i,X_j) and exit.
+(b)  Otherwise, select the X_i with the largest value of W_M(X_i)."
 }
