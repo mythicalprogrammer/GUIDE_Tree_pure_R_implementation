3666db70e57ebed3623e52df12741fdfa8817b5b
Anthony Doan <mythicalprogrammer@gmail.com>
2020-02-02 16:43:21 -0800

    Format the algorithm and add some variables.

--- 30_variable_selection.R
+++ 30_variable_selection.R
@@ -4,11 +4,20 @@ carry out the interaction tests only if all main effects are not significant.
 Letχ2_ν,α denote the upper-αquantile of the chi-squared distribution with ν d.f.
 The algorithm for variable selection can now be stated as follows"
 
-"ALGORITHM 2.1  (Variable  selection).Let K>1  be  the  number  of  non-constant
-predictor  variables  in  the  node.  Defineα=0.05/Kandβ=0.05/{K(K−1)}:
-1.  Use Procedure2.1to findWM(Xi)fori=1,2,...,K.
-2.  If maxiWM(Xi)>χ21,α, select the variable with the largest value of WM(Xi) and exit.
-3.  Otherwise,  use  Procedure2.2 to  findWI(Xi,Xj) for  each  pair  of  predictor variables:
-(a)  If  maxi=jWI(Xi,Xj)>χ21,β,
-select  the  pair  with  the  largest  value  of WI(Xi,Xj) and exit.
-(b)  Otherwise, select theXiwith the largest value of WM(Xi)."
+"ALGORITHM 2.1  (Variable  selection).
+Let K > 1  be  the  number  of  non-constant
+predictor  variables  in  the  node.
+
+Define α=0.05/K and β=0.05/{K(K−1)}:
+
+1.  Use Procedure 2.1 to find W_M(X_i) for i=1,2,...,K.
+2.  If max_i W_M(X_i) > χ2_(1,α),
+    select the variable with the largest value of W_M(X_i) and exit.
+3.  Otherwise,  use  Procedure 2.2 to  find W_I(X_i,X_j)
+    for  each  pair  of  predictor variables:
+    (a)  If  max_(i!=j) W_I(X_i,X_j) >χ2_(1,β), select  the
+         pair  with  the  largest  value  of W_I(X_i,X_j) and exit.
+    (b)  Otherwise, select the X_i with the largest value of W_M(X_i)."
+
+K <- ncol(mammograph_data) - 1 (response) #TODO automate this via formula
+alpha <- 0.05/K
